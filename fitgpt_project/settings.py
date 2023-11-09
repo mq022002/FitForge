6f2e5a936224ca81d8d34b50f12529371a4d8448
Michael Cox <maaroncox@gmail.com>
2023-11-09 11:05:27 -0500

    Template reference added to settings.py

--- fitgpt_project/settings.py
+++ fitgpt_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fitgpt_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
