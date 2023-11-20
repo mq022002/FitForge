463ea1b13b5227e87e356ea47ded4b997f7d441f
Sean Clifford <seancliff01@gmail.com>
2023-11-20 12:11:54 -0500

    added users path

--- fitgpt_project/urls.py
+++ fitgpt_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('', include("workouts.urls"))
+    path('', include("workouts.urls")),
+    path('', include("users.urls")),
 ]
