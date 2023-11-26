6abff187c73ccdce54631e60acd21897c605095d
Matthew Quijano <mq022002+professional@gmail.com>
2023-11-26 02:04:51 -0500

    Add: Models for Exercise, ExerciseInWorkout, Workout, and UserProfile
    - Exercise, just stores data from the API. We could easily change this, or anything else. Just went with the most recent API that we found.
    - ExerciseInWorkout, stores data for specific exercise in workout. For example, this would be like uhhh, tricep pushdown, 3 sets, 8 reps, with notes that could detail reminders of form, or challenges.
    - Workout, connects the Exercise and ExerciseInWorkout models.
    - UserProfile, stores the input paramters that will soon generate user workouts.

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Exercise(models.Model):
+    body_part = models.CharField(max_length=255)
+    equipment = models.CharField(max_length=255)
+    gif_url = models.URLField()
+    exercise_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    target = models.CharField(max_length=255)
+    secondary_muscles = models.JSONField()
+    instructions = models.JSONField()
+
+    def __str__(self):
+        return self.name
+
+class ExerciseInWorkout(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout = models.ForeignKey('Workout', on_delete=models.CASCADE)
+    sets = models.IntegerField(default=0)
+    reps = models.IntegerField(default=0)
+    weight = models.FloatField(null=True, blank=True)
+    notes = models.TextField(blank=True)
+
+class Workout(models.Model):
+    exercises = models.ManyToManyField(Exercise, through=ExerciseInWorkout)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fitness_goal = models.CharField(max_length=255, choices=[
+        ('Get Stronger', 'Get Stronger'),
+        ('Gain Muscle', 'Gain Muscle'),
+        ('Lose Fat', 'Lose Fat'),
+    ])
+    
+    frequency = models.IntegerField(choices=[
+        (1, '1 day per week'),
+        (2, '2 days per week'),
+        (3, '3 days per week'),
+        (4, '4 days per week'),
+        (5, '5 days per week'),
+        (6, '6 days per week'),
+        (7, '7 days per week'),
+    ], default=1)
+    workout_duration = models.IntegerField()
+    overall_intensity = models.CharField(max_length=255, choices=[
+        ('High Intensity', 'High Intensity'),
+        ('Medium Intensity', 'Medium Intensity'),
+        ('Low Intensity', 'Low Intensity'),
+    ])
+    
+    focused_muscle_groups = models.JSONField(default=list)
+    equipment_availability = models.JSONField(default=list)
+    
+    def __str__(self):
+        return self.user.username
