3df52dfce286fa169353c8b6cf95bd40d18e22eb
Matthew Quijano <mq022002+professional@gmail.com>
2023-11-26 02:20:15 -0500

    Add: Model interfaces to admin panel
    - ExerciseInWorkoutInline; displays this model in a tabular format for the selected model, being ExerciseInWorkout.
    - WorkoutAdmin; displays the fields from the ExerciseInWorkout model in an inline fashion for the workouts model. For example, this is what a row would look like: Exercises | Sets | Reps | Weight | Notes.

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Exercise, UserProfile, Workout, ExerciseInWorkout
 
-# Register your models here.
+class ExerciseInWorkoutInline(admin.TabularInline):
+    model = ExerciseInWorkout
+
+class WorkoutAdmin(admin.ModelAdmin):
+    inlines = [ExerciseInWorkoutInline]
+
+admin.site.register(Exercise)
+admin.site.register(UserProfile)
+admin.site.register(Workout, WorkoutAdmin)
