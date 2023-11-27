f60bc7e094edc3c3e7f55061039241e2f576fcb1
Sean Clifford <seancliff01@gmail.com>
2023-11-26 19:59:58 -0500

    added auth views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib import messages
+from .forms import UserCreateForm
+from django.contrib.auth import update_session_auth_hash
 
 
 # Create your views here.
@@ -10,9 +14,20 @@ def profile(request):
 
 @login_required
 def change_password(request):
-    return render(request, 'registration/password_change.html')
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        print(form.__repr__)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('profile')
+        else:
+            return render(request, 'registration/password_change.html', {'form': form, 'error': 'Invalid new password'})
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password_change.html', {'form': form})
 
-def login(request):
+def user_login(request):
     if request.user.is_authenticated:
         return redirect('profile')
     if request.method == 'POST':
@@ -22,4 +37,16 @@ def login(request):
         if user is not None:
             login(request, user)
             return redirect('profile')
-    return render(request, 'registration/login.html')
+        else:
+            return render(request, 'registration/login.html', {"error": "Incorrect Username or Password"})
+    else:
+        return render(request, 'registration/login.html')
+    
+def register(request):
+    form = UserCreateForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    return render(request, 'registration/register.html', {'form': form})
