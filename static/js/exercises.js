a98a2532edf658fa0212e6727e4b0aee6f560596
Michael Cox <95450502+michaelcox3@users.noreply.github.com>
2023-11-27 16:22:53 -0500

    added formatting to table

--- static/js/exercises.js
+++ static/js/exercises.js
@@ -1,7 +1,6 @@
 $('.exercise-row').click(function() {
     // Toggle the next element which is the hidden details
-    console.log("clicked row", this);
-    console.log($(this).next('.exercise-details').toggle());
+    $(this).next('.exercise-details').toggle();
 });
 $('#filters').submit(function(event) {
     //$('.exercise-details').hide();
