9b620f2732045256216a6e4764b3ae6c3339ddda
Matthew Quijano <mq022002+professional@gmail.com>
2023-11-28 01:27:20 -0500

    Change: Moved UserProfile model to users app
    - Removed 'equipment_availability' field for the time being.

--- workouts/models.py
+++ workouts/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 class Exercise(models.Model):
@@ -22,34 +21,3 @@ class ExerciseInWorkout(models.Model):
 
 class Workout(models.Model):
     exercises = models.ManyToManyField(Exercise, through=ExerciseInWorkout)
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fitness_goal = models.CharField(max_length=255, choices=[
-        ('Get Stronger', 'Get Stronger'),
-        ('Gain Muscle', 'Gain Muscle'),
-        ('Lose Fat', 'Lose Fat'),
-    ])
-    
-    frequency = models.IntegerField(choices=[
-        (1, '1 day per week'),
-        (2, '2 days per week'),
-        (3, '3 days per week'),
-        (4, '4 days per week'),
-        (5, '5 days per week'),
-        (6, '6 days per week'),
-        (7, '7 days per week'),
-    ], default=1)
-    
-    workout_duration = models.IntegerField()
-    overall_intensity = models.CharField(max_length=255, choices=[
-        ('High Intensity', 'High Intensity'),
-        ('Medium Intensity', 'Medium Intensity'),
-        ('Low Intensity', 'Low Intensity'),
-    ])
-    
-    focused_muscle_groups = models.JSONField(default=list)
-    equipment_availability = models.JSONField(default=list)
-    
-    def __str__(self):
-        return self.user.username
