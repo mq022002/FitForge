b6bfd56648cc294b4227d26647a1e86c343daf3f
Sean Clifford <seancliff01@gmail.com>
2023-11-28 14:45:31 -0500

    switched back to development server for flask

--- startup.py
+++ startup.py
@@ -2,7 +2,8 @@ import subprocess
 import time
 
 django = subprocess.Popen(['python', 'manage.py', 'runserver'])
-api = subprocess.Popen(['gunicorn', '-w 4', '-b', 'localhost:3000', '--chdir', 'search-api', 'web:app' ])
+#api = subprocess.Popen(['gunicorn', '-w 1', '-b', 'localhost:3000', '--chdir', 'search-api', 'web:app' ])
+api = subprocess.Popen(['python', 'search-api/web.py'])
 
 print("Starting django server and api server")
 print("Shutdown all servers with CONTROl-C\n")
