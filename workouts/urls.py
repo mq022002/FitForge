b19f9535427f09a36824b9b0fb24155fb79d7a42
Sean Clifford <seancliff01@gmail.com>
2023-12-06 14:09:37 -0500

    view workout route

--- workouts/urls.py
+++ workouts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('exercises/<str:exercise_name>', views.exercise_detail, name='exercise_detail'),
 
     path('workouts/', views.workouts, name='workouts'),
+    path('workouts/view/<int:id>', views.view_workout, name='view_workout'),
     path('workouts/create', views.create_workout, name='create_workout'),
     path('workouts/update/<int:id>', views.update_workout, name='update_workout'),
     path('workouts/delete/<int:id>', views.delete_workout, name='delete_workout'),
