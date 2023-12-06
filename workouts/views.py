eb683bc3b4ac1aa1c9b451a62045a298dac47fc9
Sean Clifford <seancliff01@gmail.com>
2023-12-06 07:04:34 -0500

    added workout form response

--- workouts/views.py
+++ workouts/views.py
@@ -2,11 +2,13 @@ from typing import Any
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from . import api
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from .forms import WorkoutForm
+from django.http import HttpResponse
+import json
 # Create your views here.
 
 def home(request):
@@ -14,19 +16,33 @@ def home(request):
     context = {'name': 'John'}
     return render(request, 'home.html', context)
 
+
 def exercises(request):
     type_list = api.exercise_types
     muscle_list = api.exercise_muscles
     difficulty_list = api.exercise_difficulties
 
+    workouts = Workout.objects.filter(user=request.user.id)
+    workout_list = []
+    for workout in workouts:
+        print(workout.id)
+        workout_list.append({'id': workout.id, 'name': workout.name})
+    print(workout_list)
+
     exercise_list = ""
     if request.method == 'POST':
-        # Use the .get() method with a default value of None
-        selected_muscle = request.POST.get('muscle', None)
-        selected_type = request.POST.get('type', None)
-        selected_difficulty = request.POST.get('difficulty', None)
-        exercise_list = api.get_exercises(muscle=selected_muscle, e_type=selected_type, difficulty=selected_difficulty, pages=3)
-
+        if 'exercise_form' in request.POST:
+            # Use the .get() method with a default value of None
+            selected_muscle = request.POST.get('muscle', None)
+            selected_type = request.POST.get('type', None)
+            selected_difficulty = request.POST.get('difficulty', None)
+            exercise_list = api.get_exercises(muscle=selected_muscle, e_type=selected_type, difficulty=selected_difficulty, pages=3)
+        elif 'workout_form' in request.POST:
+            workout_id = request.POST.get('workout_id', None)
+            workout = _workout_exercises(workout_id, request.user.id)
+            exercises = {'exercises': workout}
+            return HttpResponse(json.dumps(exercises), content_type='application/json')
+            #return render(request, 'exercises/exercises.html', {'exercises': exercise_list, 'workouts': workout_list})
         # Remove backslashes from exercise names
         # To prevent errors in routes
         # for exercise in exercise_list:
@@ -41,9 +57,22 @@ def exercises(request):
         'muscles': muscle_list,
         'types': type_list,
         'difficulties': difficulty_list,
-        'exercises': exercise_list
+        'exercises': exercise_list,
+        'workout_list': workout_list
     }
     return render(request, 'exercises/exercises.html', context)
+
+def _workout_exercises(workout_id, user_id):
+    workout_id = Workout.objects.get(id=workout_id, user=user_id).id
+    workout_exercises  = ExerciseInWorkout.objects.filter(workout_id=workout_id)
+    exercise_list = []
+    for exercise in workout_exercises:
+        exercise_data = {}
+        exercise_data.update(exercise.exercise_id.__dict__)
+        exercise_data.update(exercise.__dict__)
+        del exercise_data['_state']
+        exercise_list.append(exercise_data)
+    return exercise_list
 
 def exercise_detail(request, exercise_name):
     exercise = api.get_exercises(name=exercise_name)[0]
