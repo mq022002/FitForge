99785fc57a2a7aa2f4da775c036409bc4bf9b96f
Sean Clifford <seancliff01@gmail.com>
2023-12-06 07:02:45 -0500

    added workout selector to view workouts

--- static/js/exercises.js
+++ static/js/exercises.js
@@ -5,3 +5,38 @@ $('.exercise-row').click(function() {
 $('#filters').submit(function(event) {
     //$('.exercise-details').hide();
 });
+
+$.ajax({
+    url: "/exercises",
+    type: "POST",
+    data: {
+        'workout_form': true,
+        'workout_id': 5,
+        'csrfmiddlewaretoken': window.CSRF_TOKEN
+    },
+    success: function(data) {
+        console.log(data);
+    }
+})
+
+$('#workout-selector').on('change', (event) => {
+    $.ajax({
+        url: "/exercises",
+        type: "POST",
+        data: {
+            'workout_form': true,
+            'workout_id': event.target.value,
+            'csrfmiddlewaretoken': window.CSRF_TOKEN
+        },
+        success: function(data) {
+            $('#exercises').empty()
+            for (let exercise of data.exercises) {
+                $('#exercises').append(
+                    `<div class='border text-center lead bg-white rounded p-1 my-1'>
+                        ${exercise.name}
+                    </div>`
+                )
+            }
+        }
+    })
+})
