0948dcd990bb5e9bb4e2762765f0271cf2bfc7ec
Sean Clifford <seancliff01@gmail.com>
2023-12-08 14:13:00 -0500

    getting server ready for production

--- fitgpt_project/settings.py
+++ fitgpt_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.90.128.120', 'fitforge.club', '*']
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Application definition
