08d8d8a55adbb76fc4c4cc00ba976962428500ba
Sean Clifford <seancliff01@gmail.com>
2023-12-08 12:28:38 -0500

    moved js from template to workout.js

---
+++ static/js/workout.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(".details").click(function() {
+        if ($(this).attr("aria-expanded") == "true") {
+            $(this).html("Less Details");
+        } else {
+            $(this).html("More Details");
+        }
+    });
+
+    $('.delete').click(function() {
+        let id = $(this).data('id');
+        console.log("id", id);
+        $.ajax({
+            url: DeleteExerciseURL,
+            type: 'POST',
+            data: {
+                'csrfmiddlewaretoken': CSRFToken,
+                id: id
+            },
+            success: function(response) {
+                location.reload();
+            }
+        });
+    })
+});
