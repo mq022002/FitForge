656184e836a993c2554912d53f2ddf6a7f52696b
Sean Clifford <seancliff01@gmail.com>
2023-12-10 17:13:27 -0500

    fixed workout slug to be user workout index

--- workouts/urls.py
+++ workouts/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     path('exercises/', views.view_exercises, name='exercises'),
 
     path('workouts/', views.workouts, name='workouts'),
-    path('workouts/view/<int:id>', views.view_workout, name='view_workout'),
+    path('workouts/view/<int:workout_id>', views.view_workout, name='view_workout'),
     path('workouts/create', views.create_workout, name='create_workout'),
     path('workouts/update/<int:id>', views.update_workout, name='update_workout'),
     path('workouts/delete/<int:id>', views.delete_workout, name='delete_workout'),
     path('workouts/<str:workout_name>', views.workout, name='workout_detail'),
     path('workout/read', views.read_workout, name='read_workout'),
 
-    path('exercise-in-workout/delete/<int:id>', views.delete_exercise_in_workout, name='delete_exercise_in_workout'),
+    path('exercise-in-workout/delete/<int:workout_id>', views.delete_exercise_in_workout, name='delete_exercise_in_workout'),
     path('exercise-in-workout/create/<str:exercise_name>/', views.create_exercise_in_workout, name='create_exercise_in_workout'),
 ]
