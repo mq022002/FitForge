b6bd1207575ad7fc847e9e883f161039a30cf06f
Sean Clifford <seancliff01@gmail.com>
2023-12-13 12:25:10 -0500

    set debug to false

--- fitgpt_project/settings.py
+++ fitgpt_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['52.90.128.120', 'fitforge.club', '*']
 DEBUG_PROPAGATE_EXCEPTIONS = True
